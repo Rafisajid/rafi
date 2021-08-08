a67ca07b810c2f021e76e0ebc362b4144e94dd55
Rafisajid <rafisajid2431@gmail.com>
2021-08-08 17:24:32 +0600

    Create sajid.py

---
+++ sajid.py
@@ -0,0 +1,66 @@
+from tkinter import Y
+
+print("i am sajid")
+print("i love to play football")
+print("right now iam interested in coding")
+
+#name = "sajid"
+first_name = "rafi"
+last_name = "sajid"
+full_name = first_name +" "+ last_name
+print("hello "+full_name)
+
+age = 19
+#age = 19 + 1
+#age = age + 1
+age += 1
+print("my age is :"+str(age))
+height = 5.11
+print("my is height is :"+str(height)+"inch")
+
+human = True
+print("iam a human :"+str(human))
+#name = "rafi sajid"
+#age = 20
+#sex = "Male"
+
+name , age, sex = "rafi sajid", 20, "male"
+print(name)
+print(age)
+print(sex)
+
+name = "rafi sajid"
+#print(len(name))
+#print(name.find("s"))
+#print(name.capitalize())
+#print(name.upper())
+#print(name.lower())
+#print(name.isdigit())
+#print(name.isalpha())
+#print(name.count("a"))
+#print(name.replace("f","n"))
+#print(name*3)
+
+x = 9
+y = 2.6
+z = "5"
+
+y = int (y)
+z = int (z)
+
+x = str(x)
+y = str(y)
+
+#y = float (y)
+#z = float (z)
+#x = float (x)
+
+print(x)
+print(y)
+print(z)
+
+
+
+
+
+
