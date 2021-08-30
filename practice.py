efb0f692dc55d438c89460414d6d6f120fc64adc
Rafisajid <88551468+Rafisajid@users.noreply.github.com>
2021-08-30 19:41:56 +0600

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,110 @@
+#print("hello rafi sajid")
+#print("i love to play games")
+
+#name = "rafi sajid"
+#print("My name is : "+ (name))
+
+#first_name = "rafi"
+#last_name = "sajid"
+#full_name = first_name +" "+ last_name
+#print("My name is : "+(full_name))
+
+#age = 20
+#age += 1
+#age = age + 1
+#print("my age is :"+str(age))
+
+#height = 5.11
+#print("my height is :"+str(height)+"inch")
+
+#Human = True
+#print("you are a human :"+str(Human))
+
+#name = "rafi sajid"
+#age = 20
+#attractive = True
+
+#name , age, attractive = "bro", 21, True
+#print(name)
+#print(age)
+#print(attractive)
+
+#name = "sajid"
+
+#print(len(name))
+#print(name.find("f"))
+#print(name.capitalize())
+#print(name.upper())
+#print(name.lower())
+#print(name.isdigit())
+#print(name.isalpha())
+#print(name.count("j"))
+#print(name.replace("s","g"))
+#print(name*3)
+
+#x = 1
+#y = 11.2
+#z = "3"
+
+#x = float(x)
+#y = float(y)
+#z = float(z)
+
+#print("x is "+str(x))
+#print("Y is "+str(y))
+#print(z*3)
+
+#name = input("what's your name ?:")
+#age = int(input("your age is :"))
+#height = float(input("my height is :"))
+
+#print("assalamualykum " +(name))
+#print("iam "+str(age)+" years old")
+#print("my height is "+str(height)+"inch")
+
+#import math
+
+#pi = 3.14
+#x = 1
+#y = 2
+#z = 3
+
+#print(round(pi))
+#print(math.ceil(pi))
+#print(math.floor(pi))
+#print(abs(pi))
+#print(pow(pi,2))
+#print(math.sqrt(pi))
+#print(max(x, y, z))
+#print(min(x, y, z))
+
+
+#name = "rafi sajid"
+
+#first_name = name[:4]
+#last_name = name[:11]
+#funky_name = name[0:11:1]
+#reversed_name = name[::-1]
+#print(reversed_name)
+
+#website = "http://google.com"
+#website2 = "http://wikipedia.com"
+#slice = slice(7,-4)
+#print(website[slice])
+#print(website2[slice])
+
+age = int(input("how old are you?: "))
+
+if age == 100:
+    print("you are a century old!")
+elif age >= 18:
+    print("you are an adult!")
+elif age <= 0:
+    print("you haven't born yet!")
+else:
+    print("you are a child!")
+
+
+
+
+
