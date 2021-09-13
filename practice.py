13a87a651ba79528cf118914f138d6c4f4170ec6
Rafisajid <88551468+Rafisajid@users.noreply.github.com>
2021-09-13 17:47:56 +0600

    Update practice.py

--- practice.py
+++ practice.py
@@ -105,6 +105,144 @@ else:
     print("you are a child!")
 
 
+#temp = int(input("what is the temperature outside?: "))
+
+#if not (temp >= 0 and temp <=30):
+    #print("the temperature is bad today!")
+    #print("stay inside of home")
+#elif not (temp < 0 or temp > 30):
+    #print("the temperature is good today")
+
+#while 1==1:
+    #print("Help! I'm stuck in a loop")
+
+#name = None
+#while not name:
+  #name = input("enter your name :")
+#print("hello "+name)
+
+#for i in range(10):
+    #print(i+1)
+#for i in range (20,100+1,3):
+    #print(i)
+#for i in  ("rafi sajid"):
+   # print(i)
+
+#import time
+
+#for seconds in range(10,0,-1):
+     #print(seconds)
+     #time.sleep(1)
+#print("eid mubarak")
+
+#rows = int(input("how many rows?: "))
+#columns = int(input("how many columns?: "))
+#symbol = input("Enter a symbol to use: ")
+
+#for i in range(rows):
+    #for j in range(columns):
+        #print(symbol, end="")
+    #print()
+
+
+#while True:
+    #name = input("Enter your name : ")
+    #if name != "":
+        #break
+
+#phone_number = "018_245_90704"
+
+#for i in phone_number:
+    #if i =="_":
+        #continue
+    #print(i, end="")
+
+
+#for i in range(1,21):
+    #if i == 13:
+        #pass
+    #else:
+        #print(i)
+
+#food = ["pizza","rice","hotdog","mushroom"]
+
+#food[0] = "apple"
+
+#food.append("chicken")
+#food.remove("hotdog")
+#food.pop()
+#food.insert(0,"cake")
+#food.sort()
+#food.clear()
+
+#for i in food:
+    #print(i)
+
+
+#drinks = ["coke","water","soda"]
+#dinner = ["grill chicken","birayani","meat"]
+#dessert = ["rosho golla","rosh malaiy","tofi"]
+
+#food = [drinks,dinner,dessert]
+
+#print(food[1][2])
+
+
+#student = ("sajid",20,"Male")
+
+#print(student.count("Male"))
+#print(student.index("sajid"))
+
+#for i in student:
+    #print(i)
+
+#if "sajid"in student:
+    #print("sajid is here!")
+
+
+#utensils = {"fork","spoon","knife",}
+#dishes = {"bowl","plate","cup","knife"}
+
+#utensils.add("napkin")
+#utensils.remove("spoon")
+#utensils.clear()
+#utensils.update(dishes)
+#dinner_table = utensils.union(dishes)
+
+#print(utensils.difference(dishes))
+#print(utensils.intersection(dishes))
+
+#for i in dinner_table:
+    #print(i)
+
+
+
+#capitals = {"USA":"Washington DC", "India":"New Delhi","China":"Beijing"}
+
+#capitals.update({"Germany":"Berlin"})
+#capitals.update({"USA":"Las Vegas"})
+#capitals.pop("China")
+#capitals.clear()
+#print(capitals["USA"])
+#print(capitals.get("India"))
+#print(capitals.keys())
+#print(capitals.values())
+#print(capitals.items())
+
+#for keys,values in capitals.items():
+    #print(keys, values)
+
+
+name = "Rafi sajid"
+
+#if(name[0].islower()):
+    #name = name.capitalize()
+
+first_name = name[0:4].upper()
+last_name = name[5:].lower()
+
+print(last_name)
+
 
 
 
