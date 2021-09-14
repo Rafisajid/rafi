5127c9a557ad2be565be96ed8431754b5a408ff4
Rafisajid <88551468+Rafisajid@users.noreply.github.com>
2021-09-14 16:30:18 +0600

    Update practice.py

--- practice.py
+++ practice.py
@@ -255,3 +255,7 @@ last_symbol = name[-1]
 print(last_symbol)
 
 
+def hello(first_name,last_name,age):
+    print("Hello !"+first_name+" "+last_name)
+    print("You are "+str(age)+ " years old")
+hello("Sajid","Rafi",21 )
